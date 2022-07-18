a8fdde8b9b6dbc9d462ac47de91b47219c0c1bfe
Ross Warren <ross.warren@idexpertscorp.com>
2022-07-18 13:10:42 -0700

    Updating release variables

--- generate-release-vars.sh
+++ generate-release-vars.sh
@@ -46,7 +46,7 @@ ami_name_al1=$(aws ec2 describe-images --region "$region" --owner amazon --image
 
 # AL2
 ami_id_al2_x86=$(aws ssm get-parameters --region "$region" --names /aws/service/ami-amazon-linux-latest/amzn2-ami-minimal-hvm-x86_64-ebs --query 'Parameters[0].[Value]' --output text)
-ami_name_x86=$(aws ec2 describe-images --region "$region" --owner "679593333241" --filters "Name=name,Values=CIS Amazon Linux 2 Kernel 4.14*Level 1*" "Name=architecture,Values=x86_64" --query "sort_by(Images, &CreationDate)[-1:].[Name]" --output text)
+ami_name_al2_x86=$(aws ec2 describe-images --region "$region" --owner "679593333241" --filters "Name=name,Values=CIS Amazon Linux 2 Kernel 4.14*Level 1*" "Name=architecture,Values=x86_64" --query "sort_by(Images, &CreationDate)[-1:].[Name]" --output text)
 ami_id_al2_arm=$(aws ssm get-parameters --region "$region" --names /aws/service/ami-amazon-linux-latest/amzn2-ami-minimal-hvm-arm64-ebs --query 'Parameters[0].[Value]' --output text)
 ami_name_al2_arm=$(aws ec2 describe-images --region "$region" --owner amazon --image-id "$ami_id_al2_arm" --query 'Images[0].Name' --output text)
 
@@ -65,15 +65,15 @@ distribution_release_al2022=$(curl -s https://al2022-repos-us-west-2-9761ab97.s3
 readonly ami_name_al2_arm ami_name_al2_x86 ami_name_al1 ami_name_al2022_arm ami_name_al2022_x86 distribution_release_al2022
 
 cat >|release.auto.pkrvars.hcl <<EOF
-ami_version          = "$ami_version"
-ecs_agent_version    = "$agent_version"
-ecs_init_rev         = "$ecs_init_rev"
-docker_version       = "20.10.13"
-containerd_version   = "1.4.13"
-source_ami_al1       = "$ami_name_al1"
-source_ami_al2       = "$ami_name_al2_x86"
-source_ami_al2arm    = "$ami_name_al2_arm"
-source_ami_al2022    = "$ami_name_al2022_x86"
-source_ami_al2022arm = "$ami_name_al2022_arm"
-distribution_release_al2022  = "$distribution_release_al2022"
+ami_version                 = "$ami_version"
+ecs_agent_version           = "$agent_version"
+ecs_init_rev                = "$ecs_init_rev"
+docker_version              = "20.10.13"
+containerd_version          = "1.4.13"
+source_ami_al1              = "$ami_name_al1"
+source_ami_al2              = "$ami_name_al2_x86"
+source_ami_al2arm           = "$ami_name_al2_arm"
+source_ami_al2022           = "$ami_name_al2022_x86"
+source_ami_al2022arm        = "$ami_name_al2022_arm"
+distribution_release_al2022 = "$distribution_release_al2022"
 EOF
